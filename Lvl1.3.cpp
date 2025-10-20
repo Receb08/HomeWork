4b8340c8aa323ad40727d48aef8a86492c254a8b
Receb08 <receb.abdullabayli@gmail.com>
2025-10-20 23:21:15 +0400

    Add files via upload

---
+++ Lvl1.3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    double a;
+    cout<<"Bir ədəd daxil edin";
+    cin>>a;
+    cout<<pow(a,2)<<endl;
+    cout<<pow(a,3);
+}
